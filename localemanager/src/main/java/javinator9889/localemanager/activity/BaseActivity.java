f9d6fe92304d1017d2f425d84e589ea21cf3ee11
Javinator9889 <javialonso007@hotmail.es>
2018-12-22 18:18:50 +0100

    Updated JavaDocs (full path)

--- localemanager/src/main/java/javinator9889/localemanager/activity/BaseActivity.java
+++ localemanager/src/main/java/javinator9889/localemanager/activity/BaseActivity.java
@@ -50,17 +50,19 @@ public abstract class BaseActivity extends Activity {
 
     /**
      * Called when the activity is starting.  This is where most initialization
-     * should go: calling {@link Activity#setContentView(int)} to inflate the
-     * activity's UI, using {@link Activity#findViewById} to programmatically
-     * interact with widgets in the UI, calling {@link Activity#managedQuery(android.net.Uri,
-     * String[], String, String[], String)} to retrieve cursors for data being
-     * displayed, etc.
+     * should go: calling {@link android.app.Activity#setContentView(int)} to
+     * inflate the activity's UI, using {@link android.app.Activity#findViewById}
+     * to programmatically interact with widgets in the UI, calling {@link
+     * android.app.Activity#managedQuery(android.net.Uri, String[], String,
+     * String[], String)} to retrieve cursors for data being displayed, etc.
      *
-     * <p>You can call {@link Activity#finish} from within this function, in
+     * <p>You can call {@link android.app.Activity#finish} from within this
+     * function, in
      * which case onDestroy() will be immediately called after {@link
-     * Activity#onCreate} without any of the rest of the activity lifecycle
-     * ({@link Activity#onStart}, {@link Activity#onResume}, {@link
-     * Activity#onPause}, etc) executing.
+     * android.app.Activity#onCreate} without any of the rest of the activity
+     * lifecycle ({@link android.app.Activity#onStart}, {@link
+     * android.app.Activity#onResume}, {@link android.app.Activity#onPause},
+     * etc) executing.
      *
      * <p><em>Derived classes must call through to the super class's
      * implementation of this method.  If they do not, an exception will be
@@ -69,13 +71,13 @@ public abstract class BaseActivity extends Activity {
      * @param savedInstanceState If the activity is being re-initialized after
      *                           previously being shut down then this Bundle
      *                           contains the data it most recently supplied in
-     *                           {@link Activity#onSaveInstanceState}.
+     *                           {@link android.app.Activity#onSaveInstanceState}.
      *                           <b><i>Note: Otherwise it is null.</i></b>
      *
-     * @see Activity#onStart
-     * @see Activity#onSaveInstanceState
-     * @see Activity#onRestoreInstanceState
-     * @see Activity#onPostCreate
+     * @see android.app.Activity#onStart
+     * @see android.app.Activity#onSaveInstanceState
+     * @see android.app.Activity#onRestoreInstanceState
+     * @see android.app.Activity#onPostCreate
      */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
--- localemanager/src/main/java/javinator9889/localemanager/activity/BaseAppCompatActivity.java
+++ localemanager/src/main/java/javinator9889/localemanager/activity/BaseAppCompatActivity.java
@@ -50,18 +50,20 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity {
 
     /**
      * Called when the activity is starting.  This is where most initialization
-     * should go: calling {@link AppCompatActivity#setContentView(int)} to
-     * inflate the activity's UI, using {@link AppCompatActivity#findViewById}
+     * should go: calling {@link android.support.v7.app.AppCompatActivity#setContentView(int)}
+     * to inflate the activity's UI, using {@link android.support.v7.app.AppCompatActivity#findViewById}
      * to programmatically interact with widgets in the UI, calling {@link
-     * AppCompatActivity#managedQuery(android.net.Uri, String[], String,
-     * String[], String)} to retrieve cursors for data being displayed, etc.
+     * android.support.v7.app.AppCompatActivity#managedQuery(android.net.Uri,
+     * String[], String, String[], String)} to retrieve cursors for data being
+     * displayed, etc.
      *
-     * <p>You can call {@link AppCompatActivity#finish} from within this
+     * <p>You can call {@link android.support.v7.app.AppCompatActivity#finish}
+     * from within this
      * function, in which case onDestroy() will be immediately called after
-     * {@link AppCompatActivity#onCreate} without any of the rest of the
-     * activity lifecycle ({@link AppCompatActivity#onStart}, {@link
-     * AppCompatActivity#onResume}, {@link AppCompatActivity#onPause}, etc)
-     * executing.
+     * {@link android.support.v7.app.AppCompatActivity#onCreate} without any of
+     * the rest of the activity lifecycle ({@link android.support.v7.app.AppCompatActivity#onStart},
+     * {@link android.support.v7.app.AppCompatActivity#onResume}, {@link
+     * android.support.v7.app.AppCompatActivity#onPause}, etc) executing.
      *
      * <p><em>Derived classes must call through to the super class's
      * implementation of this method.  If they do not, an exception will be
@@ -70,13 +72,13 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity {
      * @param savedInstanceState If the activity is being re-initialized after
      *                           previously being shut down then this Bundle
      *                           contains the data it most recently supplied in
-     *                           {@link AppCompatActivity#onSaveInstanceState}.
+     *                           {@link android.support.v7.app.AppCompatActivity#onSaveInstanceState}.
      *                           <b><i>Note: Otherwise it is null.</i></b>
      *
-     * @see AppCompatActivity#onStart
-     * @see AppCompatActivity#onSaveInstanceState
-     * @see AppCompatActivity#onRestoreInstanceState
-     * @see AppCompatActivity#onPostCreate
+     * @see android.support.v7.app.AppCompatActivity#onStart
+     * @see android.support.v7.app.AppCompatActivity#onSaveInstanceState
+     * @see android.support.v7.app.AppCompatActivity#onRestoreInstanceState
+     * @see android.support.v7.app.AppCompatActivity#onPostCreate
      */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
